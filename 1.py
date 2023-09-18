94ba8fcecffecb297937899651d17631f42cb4f7
Diya R B <diyarb1411@gmail.com>
2023-09-18 13:58:34 +0530

    code01

--- 1.py
+++ 1.py
@@ -1,2 +1,6 @@
-print('hello world')
-print('123')
+a=int(input('enter the first number'))
+b=int(input('enter the second number'))
+c=int(input('enter the third number'))
+sum=a+b+c
+avg1=sum/3
+print('average:',avg1)
